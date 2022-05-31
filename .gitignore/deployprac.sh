86d760c52b4471f20744885e0e6d5e2f325587ce
ec2-user <ec2-user@gmail.com>
2022-05-31 10:55:22 +0000

    try this

---
+++ .gitignore/deployprac.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+COUNTRY=USA
+COMPANYNAME=DESAMBAGROUP-llc
+Companyawards=Employee-of-the-year
+Timestampticketresolving=less-than-5mins
+Employeesundersupervision=25
+Reportsto=Regionaloffice
+Employmentlevel=MANAGEMENT
+Manager=MASTERENGINEER-MATT
+Team=dev-programming
+subdepartment=coding
+department=IaaC
+discipline=IT
+EMPLOYEENAME=EMPLOYEENAMES
+EMPLOYEETEAM=TEAM
+EMPLOYEE=YEAR-EMPLOYED
+
+echo "--The $COMPANYNAME $discipline $department company having at its head $Manager of the $subdepartment sub-department, who reports to our $Reportsto, employed in our $Employmentlevel branch located in the $COUNTRY is an outstanding employee."
+echo "--$Manager recieved the $Companyawards recognition award for 2021 and had $Employeesundersupervision Employees under his supervision."
+
+echo "--He and his team have recorded an outstanding time-to-ticket-resolve of $Timestampticketresolving."
+echo "--It is of great importance to note here that $Manager always loggs i on time every time he gets to work."
+echo "--Also $Manager successfully escalated all difficulties the $subdepartment faced hence reducucing latency by more than 75% of the $department."
+echo "--His Agility and dexterity to accomplish and Automate tasks has contributed to raised profit for the last trimester."
+echo "--The $discipline department will now have its budget revised for the coming year as our share holders are proud of the profits&shares realized by $COMPANYNAME STOCK in the $COUNTRY stock market."
+echo "--Our $Reportsto has decided after holding a closed doors $Employmentlevel session that $COMPANYNAME will encourage other $Team especially those out of the $COUNTRY to have hiring events and showcase its progression for the past 5 years."
+echo "--Our $Reportsto is very proud of the $discipline department."
+echo "--It is a win-win for our $COUNTRY $Reportsto $subdepartment, $Team teams and  $COMPANYNAME as a whole."
+echo "--At $COMPANYNAME we are customer-centered and we fine-tune our $department via Automation for a 100% customer satisfaction."
+echo "--Below is a list of some Employees we equally are proud of."
+echo " $EMPLOYEENAME           DPT    $EMPLOYEETEAM      $EMPLOYEE     PROJECTS        EMPLOYEELEVEL           SUPERVISOR                REPORTS-TO                COUNTRY"
+echo " Matt DESAMBA            $discipline    $subdepartment     2005              $department            $Employmentlevel        $Manager            $Reportsto               $COUNTRY"
+echo " Jones Van DOUGH         $discipline    $subdepartment     2005              $department            $Employmentlevel        $Manager            $Reportsto               $COUNTRY"
+echo " Shawn DESAMBA           $discipline    $subdepartment     2005              $department            $Employmentlevel        $Manager            $Reportsto               $COUNTRY"
+echo " Jones Van DOUGH         $discipline    $subdepartment     2005              $department            $Employmentlevel        $Manager            $Reportsto               $COUNTRY"
+echo " Chris De-BURGH          $discipline    $subdepartment     2005              $department            $Employmentlevel        $Manager            $Reportsto               $COUNTRY"
+echo " Jones Van DOUGH         $discipline    $subdepartment     2005              $department            $Employmentlevel        $Manager            $Reportsto               $COUNTRY"
+echo " Emily MAYBELINE         $discipline    $subdepartment     2005              $department            $Employmentlevel        $Manager            $Reportsto               $COUNTRY"
+echo " Jones Van DOUGH         $discipline    $subdepartment     2005              $department            $Employmentlevel        $Manager            $Reportsto               $COUNTRY"
+echo " Jones Van DOUGH         $discipline    $subdepartment     2005              $department            $Employmentlevel        $Manager            $Reportsto               $COUNTRY"
+echo " Mike LANCING            $discipline    $subdepartment     2005              $department            $Employmentlevel        $Manager            $Reportsto               $COUNTRY"
+echo " Jones Van DOUGH         $discipline    $subdepartment     2005              $department            $Employmentlevel        $Manager            $Reportsto               $COUNTRY"
+echo " Jones Van DOUGH         $discipline    $subdepartment     2005              $department            $Employmentlevel        $Manager            $Reportsto               $COUNTRY"
+echo " Jones Van DOUGH         $discipline    $subdepartment     2005              $department            $Employmentlevel        $Manager            $Reportsto               $COUNTRY"
+echo " Jones Van DOUGH         $discipline    $subdepartment     2005              $department            $Employmentlevel        $Manager            $Reportsto               $COUNTRY"
+echo " Jones Van DOUGH         $discipline    $subdepartment     2005              $department            $Employmentlevel        $Manager            $Reportsto               $COUNTRY"
+echo " Evans MIKEY             $discipline    $subdepartment     2005              $department            $Employmentlevel        $Manager            $Reportsto               $COUNTRY"
+echo " Jason DORSEY            $discipline    $subdepartment     2005              $department            $Employmentlevel        $Manager            $Reportsto               $COUNTRY"
+echo " MARYLINE ALLENBOROUGH   $discipline    $subdepartment     2017              $department            $Employmentlevel        $Manager            $Reportsto               $COUNTRY"
+echo " Alexis Lancing          $discipline    $subdepartment     1989              $department            $Employmentlevel        $Manager            $Reportsto               $COUNTRY"
+echo " Marchello Diaz          $discipline    $subdepartment     2015              $department            $Employmentlevel        $Manager            $Reportsto               $COUNTRY"
+
